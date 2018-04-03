8395b4e41d4983d38a1d5a9225e6dea7cb95c503
Nithin M Warrier <nithin.warier@gmail.com>
2018-04-03 13:31:16 +0530

    Updated with hasLoop through slow & fast pointer

--- LinkedList/LinkedList2.java
+++ LinkedList/LinkedList2.java
@@ -187,5 +187,25 @@ public class LinkedList2 {
         }
 
     }
+    
+    /**
+     * Check whether the linkedList hasp loop through slow & fast pointer
+     */
+    private static boolean hasLoop(Node head) {
+      Node slow, fast; 
+      slow = fast = head; 
+      while(true) {
+        slow = slow.next; 
+        if(fast.next != null)
+            fast = fast.next.next; 
+        else
+            return false;  
+
+        if(slow == null || fast == null) 
+            return false;
+
+        if(slow == fast) 
+            return true;
+    }
 
 }
