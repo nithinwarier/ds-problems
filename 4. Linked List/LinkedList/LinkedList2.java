01933140a1e41e8f43bd7df9e776bbd8151d05b2
Nithin <nithin@aptener.com>
2018-04-22 08:54:29 +0530

    added a section for linked lists

---
+++ 4. Linked List/LinkedList/LinkedList2.java
@@ -0,0 +1,225 @@
+package com.androidians.linkedlist;
+
+/**
+ * Created by nithin on 24/02/18.
+ */
+
+public class LinkedList2 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hi Linkedlist, I am coming to you !!! Let us enjoy");
+
+        Node n = new Node(1);
+        n.next = new Node(2);
+        n.next.next = new Node(3);
+        n.next.next.next = new Node(4);
+        n.next.next.next.next = new Node(5);
+        n.next.next.next.next.next = new Node(6);
+
+        System.out.println("countIterative: "+countIterative(n));
+        System.out.println("countRecursive: "+countRecursive(n));
+        printLastNode(n);
+        System.out.println("Print LinkedList Iterative");
+        printLinkedList(n);
+        System.out.println();
+        printLinkedListRecursive(n);
+        Node middleNode = findMiddleNode(n);
+
+        System.out.println();
+        System.out.println("Middle Node: "+middleNode.data);
+        System.out.println("NthFromLastNode: "+nthFromLastNode(n, 2).data);
+        reversePrintLinkedListRecursive(n);
+        reverseLinkedList(n);
+    }
+
+    private static int countIterative(Node head) {
+        int count = 0;
+        if (head == null) return count;
+        Node p = head;
+        while (p != null) {
+            p = p.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    private static int countRecursive(Node head) {
+        if (head == null)
+            return 0;
+        else {
+            return 1 + countRecursive(head.next);
+        }
+    }
+
+    private static void printLastNode(Node head) {
+        if (head == null) return;
+        
+        Node fast = head;
+        Node slow = head;
+        Node mid = head;
+        
+        // here, we are not travelling the enitr linkedList to find the lastNode
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            mid = fast.next;
+            fast = fast.next.next;
+        }
+        
+        if (fast != null) {
+            System.out.println("LastNode: " + fast.data);
+        } else if (mid != null) {
+            System.out.println("LastNode: " + mid.data);
+        }
+    }
+
+    private static void printLinkedList(Node head) {
+        if (head == null)
+            return;
+
+        Node p = head;
+        while (p != null) {
+            System.out.print("\t"+p.data);
+            p= p.next;
+        }
+    }
+
+    private static void printLinkedListRecursive(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        System.out.print("\t"+head.data);
+        printLinkedListRecursive(head.next);
+    }
+
+    private static void reverseLinkedList(Node head) {
+        if (head == null) return;
+
+        Node prevNode = null;
+        Node currNode = head;
+        Node nextNode = null;
+
+        while (currNode != null) {
+            nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        System.out.println("prev node: "+prevNode.data);
+
+        Node p = prevNode;
+        while (p != null) {
+            System.out.print("\t"+p.data);
+            p=p.next;
+        }
+    }
+
+
+    private static void reversePrintLinkedListRecursive(Node head) {
+        if (head == null) {
+            return;
+        }
+        reversePrintLinkedListRecursive(head.next);
+        System.out.print("\t"+head.data);
+    }
+
+    private static Node findMiddleNode(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head.next;
+        int count = 0;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            count++;
+        }
+
+        return slow;
+    }
+
+    private static Node nthFromLastNode(Node head, int n) {
+        Node firstPtr = head;
+        Node secondPtr = head;
+
+        for (int i=0; i<n; i++)
+            firstPtr = firstPtr.next;
+
+        while (firstPtr != null) {
+            firstPtr = firstPtr.next;
+            secondPtr = secondPtr.next;
+        }
+
+        return secondPtr;
+    }
+    
+    private static void pairwiseSwapToReverse(Node head) {
+        if (head == null) return;
+        if (head.next == null) return;
+
+        Node p = head;
+        while (p != null && p.next != null) {
+            int k = p.data;
+            p.data = p.next.data;
+            p.next.data = k;
+            p = p.next.next;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println("\t"+temp.data);
+            temp = temp.next;
+        }
+
+    }
+    
+    /**
+     * Check whether the linkedList hasp loop through slow & fast pointer
+     */
+    private static boolean hasLoop(Node head) {
+      if (head == null) return false;
+        
+      Node slow = head;
+      Node fast = head;
+        
+      while (slow != null && fast != null && fast.next != null) {
+          slow = slow.next;
+          fast = fast.next.next;
+          
+          if (slow == fast) {
+              return true;
+          }
+      }
+      
+      return false;
+    }   
+    
+    private static boolean detectLoopAlternativeWay(Node a) {
+        HashSet<Node> set = new HashSet<>();
+        while (a != null) {
+          if (set.contains(a)) {
+              return a;
+          }
+            
+          set.add(a);
+          a = a.next;  
+      }
+      
+      return null;
+    }   
+}
---
+++ 4. Linked List/MergePointLinkedLists/MergePointTwoLinkedLists.java
@@ -0,0 +1,118 @@
+package com.androidians.sample;
+
+import org.w3c.dom.Node;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MergePointTwoLinkedLists {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node (int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node A = new Node(1);
+        A.next = new Node(2);
+        A.next.next = new Node(3);
+        A.next.next.next = new Node(4);
+
+        Node B = new Node(5);
+        B.next = new Node(8);
+        B.next.next = new Node(10);
+        B.next.next.next = new Node(3);
+        B.next.next.next.next = new Node(4);
+
+        System.out.println(mergePointTwoLinkedLists(A, B));
+    }
+
+    public static int mergePointTwoLinkedLists(Node A, Node B) {
+        int n = length(A);
+        int m = length(B);
+
+        if (n > m) {
+            A = movePointer(A, n-m);
+        } else {
+            B = movePointer(B, m-n);
+        }
+
+        while (A != null && B != null) {
+            if (A.data == B.data) {
+                return A.data;
+            }
+
+            A = A.next;
+            B = B.next;
+        }
+
+        return -1;
+    }
+
+    private static Node movePointer(Node node, int d) {
+        for (int i=0;i<d;i++) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    private static int length(Node A) {
+        Node p = A;
+        int count = 0;
+
+        while (p != null) {
+            p = p.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    //brute force - compare every node in LList A with LList B
+    public static int mergePointTwoLinkedListsBruteForce(Node A, Node B) {
+        int n = length(A);
+        int m = length(B);
+        Node temp = B;
+
+        for (int i=0;i<n; i++) {
+            B = temp;
+            for (int j=0;j<m;j++) {
+                if (A.data == B.data) {
+                    return A.data;
+                }
+
+                B = B.next;
+            }
+
+            A = A.next;
+        }
+
+        return -1;
+    }
+
+    public static int mergePointTwoLinkedListsImprovedBruteForce(Node A, Node B) {
+        int n = length(A);
+        int m = length(B);
+        Set<Integer> nodes = new TreeSet<>();
+        for (int i=0;i<m;i++) {
+            nodes.add(B.data);
+            B = B.next;
+        }
+
+        for (int i=0;i<n; i++) {
+            if (nodes.contains(A.data)) {
+                return A.data;
+            }
+
+            A = A.next;
+        }
+
+        return -1;
+    }
+}
