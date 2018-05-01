4e0799cc4656b6f0910d2b5fa31803442131985f
Nithin <nithin@aptener.com>
2018-05-01 14:04:15 +0530

    Added binary gap

---
+++ Ch 06 - Bit Manipulation/BinaryGap/BinaryGap.java
@@ -0,0 +1,25 @@
+package com.androidians.sample.codility;
+
+public class BinaryGap {
+
+    public static void main(String[] args) {
+        System.out.println(binaryGap(529));
+    }
+
+    static int binaryGap(int N) {
+        int current = 0;
+        int max = 0;
+
+        while (N > 0) {
+            if (N % 2 == 0)
+                ++current;
+            else {
+                max = Math.max(max, current);
+                current = 0;
+            }
+            N /= 2;
+        }
+        return max;
+    }
+
+}
